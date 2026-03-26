99d0e9bfafbfd4a74f87dc9ea8db21a5972c5876
Pratham Shah <prathamshah095@gmail.com>
2026-03-26 13:35:41 +0530

    Update program3.java

--- assignment1/program3.java
+++ assignment1/program3.java
@@ -1,1 +1,39 @@
 
+//Problem Statement:
+// Write a Java program demonstrating of Wrapper Classes and String vs StringBuffer
+
+public class WrapperDemo {
+    public static void main(String[] args) {
+        
+        //  Wrapper Classes Example 
+        int primitiveInt = 25;
+        // Boxing: converting primitive to object
+        Integer wrappedInt = Integer.valueOf(primitiveInt);
+        // Unboxing: converting object back to primitive
+        int unboxedInt = wrappedInt.intValue();
+        
+        System.out.println("Primitive int: " + primitiveInt);
+        System.out.println("Wrapped Integer: " + wrappedInt);
+        System.out.println("Unboxed int: " + unboxedInt);
+
+        // Other wrapper classes
+        Double wrappedDouble = Double.valueOf(15.45);
+        Character wrappedChar = Character.valueOf('k');
+        Boolean wrappedBool = Boolean.valueOf(true);
+
+        System.out.println("Wrapped Double: " + wrappedDouble);
+        System.out.println("Wrapped Character: " + wrappedChar);
+        System.out.println("Wrapped Boolean: " + wrappedBool);
+
+        // Difference String vs StringBuffer 
+        // String is immutable
+        String str = "virat";
+        str.concat(" kohli"); // does not change original string
+        System.out.println("String after concat: " + str); // still "Hello"
+
+        // StringBuffer is mutable
+        StringBuffer sb = new StringBuffer("virat");
+        sb.append(" kohli"); // modifies the original object
+        System.out.println("StringBuffer after append: " + sb); // "Hello World"
+    }
+}
